0a4b1675b4e041fd2a3c95540760c3229da4bfb5
Saibal <Saibal@ZION>
2016-10-25 18:07:43 -0700

    Changes to fetch data from Azure database

---
+++ TIH/Helpers/DatabaseManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TIH.Helpers
+{
+    public class DatabaseManager
+    {
+        private SqlDataAdapter _adapter;
+        private SqlConnection _connection;
+        private SqlCommand _command;
+
+        public DatabaseManager()
+        {
+            _adapter = new SqlDataAdapter();
+            _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["TIHAzureDbConnection"].ConnectionString);
+        }
+
+        private SqlConnection getOpenDbConnection()
+        {
+            if(_connection.State == ConnectionState.Closed || _connection.State == ConnectionState.Broken)
+            {
+                _connection.Open();
+            }
+            return _connection;
+        }
+
+        public DataTable ExecuteSelectQuery(string sqlQuery, SqlParameter[] sqlParameters)
+        {
+            DataTable dtQueryResults = new DataTable();
+            dtQueryResults = null;
+            DataSet dsQueryResults = new DataSet();
+
+            try
+            {
+                using (_command = new SqlCommand())
+                {
+                    _command.Connection = getOpenDbConnection();
+                    _command.CommandText = sqlQuery;
+                    _command.Parameters.AddRange(sqlParameters);
+                    _command.ExecuteNonQuery();
+                    _adapter.SelectCommand = _command;
+                    _adapter.Fill(dsQueryResults);
+                    dtQueryResults = dsQueryResults.Tables[0];
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (_command.Connection != null)
+                {
+                    _command.Connection.Close();
+                }
+            }
+
+            return dtQueryResults;
+        }
+    }
+}
--- TIH/HistoricalDataService.svc.cs
+++ TIH/HistoricalDataService.svc.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Net;
-using Supremes;
 using TIH.Helpers;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TIH
 {
     public class HistoricalDataService : IHistoricalDataService
     {
-        string eventsUrl = "http://www.brainyhistory.com/days/";
-        string birthdaysUrl = "http://www.brainyhistory.com/daysbirth/birth_";
-        string deathsUrl = "http://www.brainyhistory.com/daysdeath/death_";
-
-        string sanitizedUrl = "";
+        string strSqlQuery;
+        DataTable dtResults;
+        string[,] listOfResults;
 
         public string[][] GetHistoricalData(string month, string day, string type)
         {
@@ -23,40 +22,50 @@ namespace TIH
             switch (historicDataType)
             {
                 case "Events":
-                    sanitizedUrl = eventsUrl + historicMonth + "_" + historicDay + ".html";
+                    strSqlQuery = "SELECT Epoch, Event FROM Events WHERE DATEPART(MM, Epoch) = @month AND DATEPART(dd, Epoch) =@day";
                     break;
                 case "Birthdays":
-                    sanitizedUrl = birthdaysUrl + historicMonth + "_" + historicDay + ".html";
+                    strSqlQuery = "SELECT Epoch, Birthday FROM Birthdays WHERE DATEPART(MM, Epoch) = @month AND DATEPART(dd, Epoch) =@day";
+                    break;
+                case "WeddingsAndDivorces":
+                    strSqlQuery = "SELECT Epoch, Death FROM WeddingsAndDivorces WHERE DATEPART(MM, Epoch) = @month AND DATEPART(dd, Epoch) =@day";
                     break;
                 case "Deaths":
-                    sanitizedUrl = deathsUrl + historicMonth + "_" + historicDay + ".html";
+                    strSqlQuery = "SELECT Epoch, WeddingOrDivorce FROM Deaths WHERE DATEPART(MM, Epoch) = @month AND DATEPART(dd, Epoch) =@day";
                     break;
             }
 
             try
             {
-                var doc = Dcsoup.Parse(new Uri(sanitizedUrl), 5000);
-                var historySection = doc.Select("table[border=0]");
-                var dataSection = ((historySection[1].Select("td")));
+                SqlParameter[]  sqlQueryParameters =
+                                {
+                                    new SqlParameter("@month", historicMonth),
+                                    new SqlParameter("@day", historicDay)
+                                };
 
-                string[,] listOfEvents = new string[dataSection.Count/2,2];
+                DatabaseManager dbManager = new DatabaseManager();
+                dtResults = dbManager.ExecuteSelectQuery(strSqlQuery, sqlQueryParameters);
 
-                int j = 0;
-
-                for (int i = 0; i < dataSection.Count; i = i+2)
+                if (dtResults != null)
                 {
-                    listOfEvents[j,0] = dataSection[i].Text;
-                    listOfEvents[j, 1] = dataSection[i + 1].Text;
-                    j++;
-                }
+                    listOfResults = new string[dtResults.Rows.Count, 2];
 
-                return listOfEvents;
+                    for (int i = 0; i < dtResults.Rows.Count; i++)
+                    {
+                        listOfResults[i, 0] = ((System.DateTime)(dtResults.Rows[i]).ItemArray[0]).Date.ToString("MM/dd/yyyy");
+                        listOfResults[i, 1] = (String)(dtResults.Rows[i]).ItemArray[1];
+                    }
+                }
+                else
+                {
+                    throw new Exception("Error retrieving data from the database.");
+                }
             }
             catch (WebException ex)
             {
                 throw ex;
             }
+            return listOfResults;
         }
-
     }
 }
